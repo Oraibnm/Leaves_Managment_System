aeb09f116f92c5750377600168a28f10730a7051
Oraib Nidal Maswadeh <35704060+Oraibnm@users.noreply.github.com>
2022-09-07 15:30:28 +0300

    commit

---
+++ add_employee.php
@@ -0,0 +1,95 @@
+<?php
+require('top.inc.php');
+$name='';
+$email='';
+$department_id='';
+$address='';
+$id='';
+if(isset($_GET['id'])){
+	$id=mysqli_real_escape_string($con,$_GET['id']);
+	if($_SESSION['ROLE']==2 && $_SESSION['USER_ID']!=$id){
+		die('Access denied');
+	}
+	$res=mysqli_query($con,"select * from employee where id='$id'");
+	$row=mysqli_fetch_assoc($res);
+	$name=$row['name'];
+	$email=$row['email'];
+	$department_id=$row['department_id'];
+	$address=$row['address'];
+	
+}
+if(isset($_POST['submit'])){
+	$name=mysqli_real_escape_string($con,$_POST['name']);
+	$email=mysqli_real_escape_string($con,$_POST['email']);
+	$password=mysqli_real_escape_string($con,$_POST['password']);
+	$department_id=mysqli_real_escape_string($con,$_POST['department_id']);
+	$address=mysqli_real_escape_string($con,$_POST['address']);
+
+	if($id>0){
+		$sql="update employee set name='$name',email='$email',password='$password',department_id='$department_id',address='$address' where id='$id'";
+	}else{
+		$sql="insert into employee(name,email,password,department_id,address,role) values('$name','$email','$password','$department_id','$address', '2')";
+	}
+	mysqli_query($con,$sql);
+	header('location:employee.php');
+	die();
+}
+?>
+<div class="content pb-0">
+            <div class="animated fadeIn">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="card">
+                        <div class="card-header"><strong>Leave Type</strong><small> Form</small></div>
+                        <div class="card-body card-block">
+                           <form method="post">
+							   <div class="form-group">
+									<label class=" form-control-label">Name</label>
+									<input type="text" value="<?php echo $name?>" name="name" placeholder="Enter employee name" class="form-control" required>
+								</div>
+								<div class="form-group">
+									<label class=" form-control-label">Email</label>
+									<input type="email" value="<?php echo $email?>" name="email" placeholder="Enter employee email" class="form-control" required>
+								</div>
+								
+								<div class="form-group">
+									<label class=" form-control-label">Password</label>
+									<input type="password"  name="password" placeholder="Enter employee password" class="form-control" required>
+								</div>
+								<div class="form-group">
+									<label class=" form-control-label">Department</label>
+									<select name="department_id" required class="form-control">
+										<option value="">Select Department</option>
+										<?php
+										$res=mysqli_query($con,"select * from department order by department desc");
+										while($row=mysqli_fetch_assoc($res)){
+											if($department_id==$row['id']){
+												echo "<option selected='selected' value=".$row['id'].">".$row['department']."</option>";
+											}else{
+												echo "<option value=".$row['id'].">".$row['department']."</option>";
+											}
+										}
+										?>
+									</select>
+								</div>
+								<div class="form-group">
+									<label class=" form-control-label">Address</label>
+									<input type="text" value="<?php echo $address?>" name="address" placeholder="Enter employee address" class="form-control" required>
+								</div>
+								
+							   <?php if($_SESSION['ROLE']==1){?>
+							   <button  type="submit" name="submit" class="btn btn-lg btn-info btn-block">
+							   <span id="payment-button-amount">Submit</span>
+							   </button>
+							   <?php } ?>
+							  </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+                  
+<?php
+require('footer.inc.php');
+?>
---
+++ db.inc.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','','emp_leaves_system');
+
+?>
